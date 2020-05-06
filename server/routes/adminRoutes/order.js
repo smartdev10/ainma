78a69988ee204ca565e9c3bb9ff220371d053a96
Abdel <abdeljalil@vivaldi.net>
2020-05-06 14:14:52 +0000

    updated

--- server/routes/adminRoutes/order.js
+++ server/routes/adminRoutes/order.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const Order = require('../../controllers/adminControllers/orders')
-
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,'../../images')
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().toISOString() + '-' + file.originalname )
+    }
+})
+const multerUploads = multer({ storage }).single('money_transfer_image');
 //admin routes
 router.route("/").get(Order.getOrders);
-router.route("/").post(Order.createOrder);
+router.route("/").post(multerUploads,Order.createOrder);
 router.route("/").delete(Order.deleteOrder);
 router.route("/:id").get(Order.getOneOrder);
 router.route("/:id").put(Order.updateOrder);
