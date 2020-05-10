fa660f6894c407e152af5c03395fe35b349c6508
Abdel <abdeljalil@vivaldi.net>
2020-05-10 23:59:12 +0000

    updated

--- server/controllers/adminControllers/orders.js
+++ server/controllers/adminControllers/orders.js
@@ -1,7 +1,10 @@
 const db = require("../../models");
 const mongoose = require("mongoose");
-
-const { Order } = db;
+const isDataUri = require('validator/lib/isDataURI')
+const mime = require('mime');
+const fs = require('fs');
+const dataUriToBuffer = require('data-uri-to-buffer');
+const { Order , User } = db;
 
 class Orders {
   static async getOrders(req, res,next) {
@@ -29,18 +32,43 @@ class Orders {
    }
   static async createOrder(req, res,next) {
     try {
-         const {user} = req.body
-         if(mongoose.isValidObjectId(user)){
-          await Order.create(req.body)
-          return res.status(200).json({
-           status:200,
-           message : "saved with success"
-          });
+         const {user , money_transfer_image , items} = req.body
+         if(mongoose.isValidObjectId(user) && isDataUri(money_transfer_image)){
+          let foundUser = await User.findOne({_id:user})
+          let check = items.every(item => mongoose.isValidObjectId(item.product) && mongoose.isValidObjectId(item.place))
+          if(check){
+            if(foundUser){
+              var decodedImg = dataUriToBuffer(money_transfer_image);
+              let raw = new Buffer.from(decodedImg, 'base64');
+              let type = decodedImg.type;
+              let extension = mime.getExtension(type);
+              let fileName = `image_${foundUser.name}_receipt.` + extension
+              fs.writeFileSync("./images/receipts/" + fileName, raw, 'utf8');
+              await Order.create({...req.body,money_transfer_image:fileName})
+              return res.status(200).json({
+               status:200,
+               message : "saved with success"
+              });
+            }else{
+              return next({
+                status :400,
+                errorStatus:3,
+                message:"user not found"
+               })
+            }
+          }
+          else{
+            return next({
+              status :400,
+              errorStatus:1,
+              message:"invalid items ids"
+             })
+          }
          }else{
            return next({
             status :400,
-            errorStatus:3,
-            message:error.message
+            errorStatus:5,
+            message:"invalid data"
            })
          }
      } catch (error) {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const UsersAuth = require('../controllers/authUser');
 const AdminUsersAuth = require('../controllers/authAdmin');
-const { multerUploads } = require('../utils/multerUploader');
-
 const router = express.Router();
 
 // admins
